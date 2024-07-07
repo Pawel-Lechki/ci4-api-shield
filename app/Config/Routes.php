dbc9fc94ae7d0239e133b3f56d5136065e03fde5
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2024-07-07 12:03:13 +0200

    use route namepsaces

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -24,12 +24,25 @@ $routes->put("update-student", "Sites::updateStudent");
 
 $routes->delete("delete-student", "Sites::deleteStudent");
 
-$routes->group("product", function ($routes) {
-  $routes->post("add", "Poduct::addProduct");
-  $routes->get("list", "Poduct::listPoducts");
+// $routes->group("product", function ($routes) {
+//   $routes->post("add", "Poduct::addProduct");
+//   $routes->get("list", "Poduct::listPoducts");
+// });
+
+// $routes->group("category", function ($routes) {
+//   $routes->get("list", "Category::listCategory");
+//   $routes->post("add", "Poduct::addCategory");
+// });
+
+// Admin
+$routes->group("admin", ["namespace" => "App\Controllers\Admin"], function ($routes) {
+  $routes->get("set-role", "AdminController::setRole");
+  $routes->get("update-profile", "AdminController::updateProfile");
+  $routes->get("add-user", "AdminController::addUser");
 });
 
-$routes->group("category", function ($routes) {
-  $routes->get("list", "Category::listCategory");
-  $routes->post("add", "Poduct::addCategory");
+// Seller
+$routes->group("seller", ["namespace" => "App\Controllers\Seller"], function ($routes) {
+  $routes->get("add-product", "SellerController::addPordact");
+  $routes->get("add-category", "SellerController::addCategory");
 });
